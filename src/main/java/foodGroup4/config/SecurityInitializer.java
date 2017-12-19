5986039eb6361c59a5424e204919d203e8bf640a
Truong Sinh <ntruongsinh96@gmail.com>
2017-12-19 16:37:32 +0700

    Spring security
    Cần j thêm thì xem file xml để cấu hình nghen, mấy cái t ghi để cho m bk cái nào để sửa thôi, dùng lệnh nào thôi

--- src/main/java/foodGroup4/config/SecurityInitializer.java
+++
@@ -1,7 +0,0 @@
-package foodGroup4.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-
-}
