41fc2211288709a795ecc809eedd19d2b19e0c57
Unknown <nnthienbao040@gmail.com>
2017-12-23 23:25:58 +0700

    Thay đổi mật khẩu

--- src/main/java/foodGroup4/controller/CustomerController.java
+++ src/main/java/foodGroup4/controller/CustomerController.java
@@ -2,13 +2,18 @@ package foodGroup4.controller;
 
 import javax.validation.Valid;
 
+import foodGroup4.common.ChangePasswordCustomerValidator;
 import foodGroup4.common.ChangeProfileCustomerValidator;
+import foodGroup4.dto.ChangePasswordDto;
 import foodGroup4.dto.KhachhangDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -30,6 +35,12 @@ public class CustomerController {
 
 	@Autowired
     ChangeProfileCustomerValidator changeProfileCustomerValidator;
+
+	@Autowired
+    ChangePasswordCustomerValidator changePasswordCustomerValidator;
+
+	@Autowired
+    BCryptPasswordEncoder passwordEncoder;
 
 	public void setCustomerService(CustomerService customerService) {
 		this.customerService = customerService;
@@ -68,7 +79,58 @@ public class CustomerController {
 	}
 
 	@RequestMapping(value = "/change-password", method = RequestMethod.GET)
-	public String changePassword(Model model) {
+	public String getChangePassword(Model model) {
+	    model.addAttribute("changepassword", new ChangePasswordDto());
 		return "change-password";
 	}
+
+	@RequestMapping(value = "/change-password", method = RequestMethod.POST)
+    public String postChangePassword(
+            @ModelAttribute(value = "changepassword") ChangePasswordDto changePasswordDto,
+            BindingResult result,
+            Principal principal,
+            RedirectAttributes redirectAttributes) {
+
+	    changePasswordCustomerValidator.validate(changePasswordDto, result);
+
+        handleChangePassword(principal.getName(), changePasswordDto, result);
+
+	    if(result.hasErrors()) {
+	        return "change-password";
+        }
+
+        redirectAttributes.addFlashAttribute("message", "Thay đổi mật khẩu thành công");
+
+	    return "redirect:/customer/profile";
+    }
+
+    private void handleChangePassword(String sdtKhachhang, ChangePasswordDto changePasswordDto, Errors errors) {
+	    if(errors.hasErrors()) {
+	        return;
+        }
+	    Khachhang khachhang = customerService.findBySdt(sdtKhachhang);
+	    if(!passwordEncoder.matches(changePasswordDto.getOldPassword(), khachhang.getPassword())) {
+	        errors.rejectValue("oldPassword", "khachhang.oldpassword.invalid");
+	        return;
+        }
+
+        khachhang.setPassword(passwordEncoder.encode(changePasswordDto.getNewPassword()));
+
+	    customerService.update(khachhang);
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
